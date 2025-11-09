ed17721b55ad3d2579274bd7fc02734e584dc34b
Santosh Singh <santosh1808.singh@gmail.com>
2025-11-09 13:10:49 +0530

    Update main.tf

--- terraform-todoapp-infra/modules/azurerm_container_registry/main.tf
+++ terraform-todoapp-infra/modules/azurerm_container_registry/main.tf
@@ -7,9 +7,6 @@ resource "azurerm_container_registry" "acr" {
   tags                = var.tags
 }
 
-resource "azurerm_role_assignment" "sony_acr_pull" {
-  depends_on           = [azurerm_container_registry.acr]
-  principal_id         = azurerm_kubernetes_cluster.sony_cluster.kubelet_identity[0].object_id
-  role_definition_name = "AcrPull"
-  scope                = azurerm_container_registry.acr.id
+output "acr_id" {
+  value = azurerm_container_registry.acr.id
 }
